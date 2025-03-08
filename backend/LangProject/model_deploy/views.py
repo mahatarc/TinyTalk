fix: remove pixel overflow

backend/LangProject/model_deploy/views.py
@@ -11,7 +11,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 # Load fine-tuned model (Ensure correct path)
-MODEL_PATH = "D:/TinyTalk/fine_tuned_model"  # Update this path if needed
+MODEL_PATH = "D:/Major_Project/TinyTalkProject/fine_tuned_model"  # Update this path if needed
 processor = Wav2Vec2Processor.from_pretrained(MODEL_PATH)
 model = Wav2Vec2ForSequenceClassification.from_pretrained(MODEL_PATH)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")