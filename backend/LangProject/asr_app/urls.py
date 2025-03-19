06b07ba75abdd36dd57ac813b0d890a35ede5914
mahatarc <archanamahat7@gmail.com>
2025-03-19 12:53:27 +0545

    fix: add final model

--- backend/LangProject/asr_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views  # Import views from the same app
-
-urlpatterns = [
-    path("evaluate_speech_asr/", views.evaluate_speech_asr, name="evaluate_speech_asr"),  # Correct URL pattern
-]
